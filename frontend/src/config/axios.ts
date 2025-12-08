73c0849097e07402e2a377019404d3764c367280
Edoardo Cavallo <iweek16@gmail.com>
2025-12-08 15:44:23 +0100

    Move .env in root

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const PORT = '10000'
-const BASE_URL = `http://192.168.1.249:${PORT}/api`;
+// const PORT = '10000'
+const BASE_URL = `/api`;
 
 
 const api = axios.create({
